dd8e87025876c3277164bfe254ba5e8e26db77ac
Danila <Danila@DESKTOP-44LP0PV>
2026-05-17 17:46:41 +0300

    DetailedSolution

---
+++ SubnetCalculator/HelpWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SubnetCalculator
+{
+
+	public partial class HelpWindow : Window
+	{
+		public HelpWindow(string title, string content)
+		{
+			InitializeComponent();
+			this.Title = Title;
+			txtHelpContent.Text = content;
+		}
+
+		private void Btn_Ok_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+	}
+}
